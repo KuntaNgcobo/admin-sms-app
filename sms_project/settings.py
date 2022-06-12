965f05711a9b68f683c464ea34c496813176c8d4
KuntaNgcobo <NGCTHA030@myuct.ac.za>
2022-06-12 22:53:47 +0200

    fix added hosts

--- sms_project/settings.py
+++ sms_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://sender-sms-app.herokuapp.com/", "127.0.0.1"]
+ALLOWED_HOSTS = ["sender-sms-app.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
